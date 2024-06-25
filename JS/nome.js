2ac3151fce6185d157b5c51f32dd61aa710aa8ad
Giovanni Pascuotte da Silva <100391366+gean12390@users.noreply.github.com>
2024-06-25 15:49:59 -0300

    Delete JS directory

--- JS/nome.js
+++
@@ -1,9 +0,0 @@
-
-if (window.matchMedia('(max-width: 450px)').matches) {
-    document.querySelector('.titulo').style.marginLeft  = '37vw';
-    document.querySelector('.titulo').style.fontSize  = '35px';
-    document.querySelector('.titulo').innerHTML = 'TMSB';
-
-} else {
-    document.querySelector('.titulo').innerHTML = 'Thunderstrike Music Store Brazil';
-}
